187bae3eb334e0e6c114ed3f49b1616ae6f01d83
Endy Muhardin <endy.muhardin@gmail.com>
2018-10-15 21:36:09 +0700

    implement login with google

--- src/main/java/com/muhardin/endy/belajar/sso/googlesso/controller/HomeController.java
+++ src/main/java/com/muhardin/endy/belajar/sso/googlesso/controller/HomeController.java
@@ -1,9 +1,11 @@
 package com.muhardin.endy.belajar.sso.googlesso.controller;
 
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/")
@@ -11,6 +13,12 @@ public class HomeController {
 
     @GetMapping("home")
     public void home(){}
+
+    @GetMapping("auth")
+    @ResponseBody
+    public Authentication auth(Authentication auth) {
+        return auth;
+    }
 
     @PreAuthorize("hasAuthority('VIEW_REKENING')")
     @GetMapping("rekening")
