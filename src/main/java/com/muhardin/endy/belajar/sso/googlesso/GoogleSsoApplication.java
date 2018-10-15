357286e416f6a59612ef463806def15524d365c5
Endy Muhardin <endy.muhardin@gmail.com>
2018-10-15 13:26:32 +0700

    starter project

---
+++ src/main/java/com/muhardin/endy/belajar/sso/googlesso/GoogleSsoApplication.java
@@ -0,0 +1,12 @@
+package com.muhardin.endy.belajar.sso.googlesso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleSsoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleSsoApplication.class, args);
+	}
+}
